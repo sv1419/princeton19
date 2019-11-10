69940b1d8a001660071391fddab0c4fafa8e9ec4
sv1419 <sv1419@nyu.edu>
2019-11-10 06:31:00 -0500

    index

--- src/Components/friendProfile.js
+++ src/Components/friendProfile.js
@@ -21,6 +21,42 @@ function friendProfile (props){
     console.log(Http.responseText)
 }
   }
+
+  var map, infoWindow;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 20
+      });
+      infoWindow = new google.maps.InfoWindow;
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    }
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    }
+
+
   return(
     <div>
       <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
-
 import '../App.css';
 import '../App2.css';
-import logo from './img/DRIFT-logo.png';
+import logo from './img/gluuLogo.png';
 import '../fontawesome-free/css/all.min.css';
 
 
-
-
-class Nav extends Component {
+class Home extends Component{
   constructor(props) {
     super(props);
     this.toggleNavbar = this.toggleNavbar.bind(this);
@@ -23,91 +19,43 @@ class Nav extends Component {
       collapsed: !this.state.collapsed,
     });
   }
-  render() {
+  render(){
     const collapsed = this.state.collapsed;
 
     const classOne = collapsed ? 'collapse navbar-collapse text-uppercase' : 'collapse navbar-collapse show';
     const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
-    return (
-    <div>
-    <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top">
-    <div className="container" class="">
-      <a className="navbar-brand" href="#"></a>
-        <div align="left">
-        <button onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          &nbsp;
-          <i class="fas fa-bars"></i>
-          <span className="navbar-toggler-icon" />
-        </button>
-        </div>
-
-        <div className={`${classOne}`} id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">My Account</a>
-            </li>
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">About</a>
-            </li>
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Contact</a>
-            </li>
-          </ul>
-        </div>
-    </div>
-  </nav>
-  <header class="masthead bg-primary text-white text-center">
-    <div class="container d-flex align-items-center flex-column">
-
-      <img class="masthead-avatar mb-5" src={logo} alt="logo"></img>
-
-      <p class="masthead-subheading font-weight-light mb-0">Keep Your Phone Close And Your Friends Closer</p>
-
-      <div class="divider-custom divider-light">
-        <div class="container">
-          <a href="#"><button type="submit" class="btn btn-secondary btn-xl btn-block" id="sendMessageButton">Log In</button></a>
-          &nbsp;
-          <a href={'/signUp'}><button type="submit" class="btn btn-secondary btn-xl btn-block" id="sendMessageButton">Sign Up</button></a>
-        </div>
-      </div>
-    </div>
-  </header>
-</div>
-
-)
-}
-}
-export default Nav;
-
-/*class Home extends Component{
-  render(){
     return(
-      <div>
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-          <div class="container">
-            <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              &nbsp;
-              <i class="fas fa-bars"></i>
-            </button>
-          </div>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item mx-0 mx-lg-1">
-                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">My Account</a>
-              </li>
-              <li class="nav-item mx-0 mx-lg-1">
-                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">About</a>
-              </li>
-              <li class="nav-item mx-0 mx-lg-1">
-                <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Contact</a>
-              </li>
-            </ul>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top">
+          <div className="container" class="">
+            <a className="navbar-brand" href="#"></a>
+              <div align="left">
+              <button onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                &nbsp;
+                <i class="fas fa-bars"></i>
+                <span className="navbar-toggler-icon" />
+              </button>
+              </div>
+
+              <div className={`${classOne}`} id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">My Account</a>
+                  </li>
+                  <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">About</a>
+                  </li>
+                  <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Contact</a>
+                  </li>
+                </ul>
+              </div>
           </div>
         </nav>
 
-        <header class="masthead bg-primary text-white text-center">
+
+        <header class="masthead text-center">
           <div class="container d-flex align-items-center flex-column">
 
             <img class="masthead-avatar mb-5" src={logo} alt="logo"></img>
@@ -116,16 +64,16 @@ export default Nav;
 
             <div class="divider-custom divider-light">
               <div class="container">
-                <a href="#"><button type="submit" class="btn btn-secondary btn-xl btn-block" id="sendMessageButton">Log In</button></a>
+                <a href="#" class="button1">Log In</a>
                 &nbsp;
-                <a href={'/signUp'}><button type="submit" class="btn btn-secondary btn-xl btn-block" id="sendMessageButton">Sign Up</button></a>
+                <a href={'/signUp'} class="button1">Sign Up</a>
               </div>
             </div>
           </div>
         </header>
-      </div>
 
+      </div>
     )
   }
 }
-export default Home;*/
+export default Home;

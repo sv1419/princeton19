06e446be4e79d5cae04a355f96bd6a3ceb49d578
cloudberries27 <ccrod2298@gmail.com>
2019-11-10 08:37:35 -0500

    working on css

--- src/Components/signUp.js
+++ src/Components/signUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 import '../fontawesome-free/css/all.min.css';
 import logo from './img/gluuLogo.png';
@@ -13,6 +14,15 @@ class signUp extends React.Component{
      email: "",
      fullname: ""
     };
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true,
+    };
+  }
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
   }
   updateInput = e => {
     this.setState({
@@ -35,37 +45,46 @@ class signUp extends React.Component{
     });
   };
   render(){
+    const collapsed = this.state.collapsed;
+
+    const classOne = collapsed ? 'collapse navbar-collapse text-uppercase' : 'collapse navbar-collapse show';
+    const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
     return(
-      <div>
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-          <div class="container">
-            <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              &nbsp;
-              <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item mx-0 mx-lg-1">
-                  <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">My Account</a>
-                </li>
-                <li class="nav-item mx-0 mx-lg-1">
-                  <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
-                </li>
-                <li class="nav-item mx-0 mx-lg-1">
-                  <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
-                </li>
-              </ul>
-            </div>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top">
+          <div className="container" class="">
+            <a className="navbar-brand" href="#"></a>
+              <div align="left">
+              <button onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                &nbsp;
+                <i class="fas fa-bars"></i>
+                <span className="navbar-toggler-icon" />
+              </button>
+              </div>
+
+              <div className={`${classOne}`} id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">My Account</a>
+                  </li>
+                  <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">About</a>
+                  </li>
+                  <li class="nav-item mx-0 mx-lg-1">
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Contact</a>
+                  </li>
+                </ul>
+              </div>
           </div>
         </nav>
 
-        <header class="masthead bg-primary text-white text-center">
+        <header class="masthead text-center">
           <div class="container d-flex align-items-center flex-column">
             <img class="masthead-avatar mb-5" src={logo} alt=""></img>
 
             <div class="divider-custom divider-light">
-              <form onSubmit={this.addUser}>
+              <form onSubmit={this.addUser} action={'/profile'}>
                 Full Name:<br></br>
                 <input type="text" name="fullname" id="name" onChange={this.updateInput} value={this.state.fullname}></input><br></br>
                 Email:<br></br>
@@ -74,13 +93,11 @@ class signUp extends React.Component{
                 <input type="text" name="password" id="password"></input><br></br>
                 Phone Number:<br></br>
                 <input type="text" name="phone" id="phone"></input><br></br>
-                <input type="submit" value="Submit"></input>
+                <br></br>
+                <a href={'/profile'} class="button1">Sign Up</a>
+
               </form>
               <br></br>
-              <div class="container">
-                  <a href={'/profile'}><button type="submit" class="btn btn-secondary btn-xl btn-block" id="sendMessageButton">Log In</button></a>
-                  &nbsp;
-              </div>
             </div>
           </div>
         </header>

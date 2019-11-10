7c980302f372316c18dfe4a63661a987bb43728a
cloudberries27 <ccrod2298@gmail.com>
2019-11-09 21:30:37 -0500

    testing

--- src/Components/signUp.js
+++ src/Components/signUp.js
@@ -2,9 +2,30 @@ import React from 'react';
 import '../App.css';
 import '../fontawesome-free/css/all.min.css';
 import logo from './img/DRIFT-logo.png';
+const {OAuth2Client} = require('google-auth-library');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
 
+const responseGoogle = (response) => {
+  console.log(response);
+}
+ReactDOM.render(
+  <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    render={renderProps => (
+      <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+    )}
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />,
+  document.getElementById('googleButton')
+);
 
 function signUp (props){
+
   return(
     <div>
       <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
